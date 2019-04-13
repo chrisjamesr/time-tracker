c946214e325d799cf9ed0a1e6ac6cfa623b17de7
chrisjamesr <chrisjames.r@gmail.com>
2019-04-12 20:27:05 -0400

    add window file, style sheet, and markup for dashboard window

---
+++ renderer/dashboard/dash-window.js
@@ -0,0 +1,33 @@
+const {app, BrowserWindow} = require('electron');
+const electron = require('electron')
+const path = require('path');
+
+function createDashWindow(options){
+  console.log('creating dashboard window')
+
+  dashWindow = new BrowserWindow({
+    width: 300, 
+    height:200,
+    // x: electron.screen.getPrimaryDisplay().bounds.width,
+    // y: 0,
+    fullscreenable: true,
+    title: 'Task Dashboard',
+    resizable: false,
+    maximizable: false,
+    alwaysOnTop:false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  dashWindow.loadURL(path.join('file://', __dirname, 'dashboard.html'));
+  dashWindow.on('close', ()=>{
+    dashWindow = null;
+    console.log('dashboard window closed')
+  })
+  return dashWindow;
+}
+
+
+
+module.exports = createDashWindow
