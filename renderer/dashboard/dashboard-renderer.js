8bfb2d89bfd3a296937daac39bae190be0d09d5b
chrisjamesr <chrisjames.r@gmail.com>
2019-04-14 18:43:56 -0400

    add file for dashboard render process

---
+++ renderer/dashboard/dashboard-renderer.js
@@ -0,0 +1,37 @@
+const { ipcRenderer, BrowserWindow } = require('electron');
+
+let taskUl = document.getElementById('tasks-list')
+
+// ipcRenderer.send("show-dashboard")
+
+ipcRenderer.on("load-dashboard", (e,{tasks})=>{
+  console.log(tasks)
+
+  if (!tasks.length) return taskUl.append(document.createTextNode("no tasks to display"))
+  tasks.forEach(t=>{
+    taskUl.appendChild(createTaskLi(t))
+  })
+})
+
+function createTaskLi(t){
+  let task = document.createElement('li')
+
+  task.value = t._id
+  task.id = t.taskName.split(' ').map(t=> t.toLowerCase()).join('-')
+  task.classList.add('task-li')
+  return task
+}
+
+function createTaskLiContent({taskName, duration}){
+  let taskName = document.createElement('span')
+  taskName.innerText = t.taskName
+  taskName.classList.add('task-li-name')
+  let taskDuration = document.createElement('span')
+  taskDuration.innerText = 0
+  taskDuration.classList.add('task-li-duration')
+  return {taskName, taskDuration}
+}
+
+function addNodesToLi(){
+  
+}
