6752650c2b5fbe88648379aa39db46407bc2118a
chrisjamesr <chrisjames.r@gmail.com>
2019-04-16 13:10:02 -0400

    implement findOrCreateTask inplace of find and insert

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const path = require('path');
-const {insertTask, insertSpan, updateSpan, loadTasks, findTask} = require('./lib/db.js')
+const {findOrCreateTask, insertTask, insertSpan, updateSpan, loadTasks, findTask} = require('./lib/db.js')
+// const {findOrCreateTask, insertTask, insertSpan, updateSpan, loadTasks, findTask} = findOrCreateTaske('./lib/db.js')
 const createMainWindow = require('./renderer/window.js')
 const createDashWindow = require('./renderer/dashboard/dash-window.js')
 // state variables
@@ -19,10 +20,10 @@ app.on('ready', ()=>{
     });
 });
 
-ipcMain.on("start-counter", (e, {taskName, startTime})=>{
+ipcMain.on("start-counter", (e, {taskName, taskId, startTime})=>{
   counting = true
-
-  insertSpan(taskName, startTime)
+  console.log(`${taskName}: ${taskId} : ${startTime} `)
+  insertSpan(taskName, taskId, startTime)
     .then(taskSpan=> {
       e.sender.send("start-counter",{
         spanId: taskSpan._id,
@@ -42,14 +43,14 @@ ipcMain.on('populate-task-select', (e)=>{
 
 ipcMain.on("stop-counter", (e, args)=>{
   counting = false
-  let {taskName, stopTime, spanId} = args;  
+  let {taskId, stopTime, spanId} = args;  
   e.sender.send("stop-counter",{counting});
   updateSpan(args)
 });
 
 //not in use, sends task loaded from db
 ipcMain.on("task-selection",(e,{task})=>{
-  findTask(task)
+  findOrCreateTask(task)
     .then((t)=>{
       return t
     })
@@ -57,7 +58,8 @@ ipcMain.on("task-selection",(e,{task})=>{
 })
 
 ipcMain.on("create-new-task", (e, {task}) => {
-  insertTask( {taskName:task} )
+  // insertTask( {taskName:task} )
+  findOrCreateTask( {taskName:task} )
   .then(newTask=> {
     e.sender.send("create-new-task", { task: newTask })
   });
