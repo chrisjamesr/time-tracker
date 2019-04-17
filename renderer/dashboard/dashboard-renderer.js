8ac323bcc49791a50b84e8904d3f0de35fb730b6
chrisjamesr <chrisjames.r@gmail.com>
2019-04-16 20:52:33 -0400

    modify load-dashboard listener to parse new taskSpan object configuration

--- renderer/dashboard/dashboard-renderer.js
+++ renderer/dashboard/dashboard-renderer.js
@@ -1,45 +1,57 @@
 const { ipcRenderer, BrowserWindow } = require('electron');
 
-let taskUl = document.getElementById('tasks-list')
+let taskUl = document.getElementById('tasks-list') 
 
-// ipcRenderer.send("show-dashboard")
-
-ipcRenderer.on("load-dashboard", (e,{tasks})=>{
+ipcRenderer.on("load-dashboard", (e,tasks) => {
   console.log(tasks)
-
-  if (!tasks.length) return taskUl.append(document.createTextNode("no tasks to display"))
-  tasks.forEach(t=>{
+  if (!Object.entries(tasks).length) return taskUl.append(document.createTextNode("no tasks to display"))
+  Object.entries(tasks).forEach( t => {
+    console.log(t);
     taskUl.appendChild(createTaskLi(t))
   })
 })
 
 function createTaskLi(t){
+  debugger
+  let [taskName, {taskId, duration}] = t
   let task = document.createElement('li')
-  task.value = t._id
-  task.id = t.taskName.split(' ').map(t=> t.toLowerCase()).join('-')
+  task.value = taskId
+  // task.id = t.taskName.split(' ').map(t=> t.toLowerCase()).join('-')
   task.classList.add('task-li')
 
-  let taskName = document.createElement('span')
-  taskName.innerText = t.taskName
-  taskName.classList.add('task-li-name')
+  let taskNameSpan = document.createElement('span')
+  taskNameSpan.innerText = taskName
+  taskNameSpan.classList.add('task-li-name')
   
-  let taskDuration = document.createElement('span')
-  taskDuration.innerText = 0
-  taskDuration.classList.add('task-li-duration')
+  let taskDurationSpan = document.createElement('span')
+  taskDurationSpan.innerText = `${duration/1000} seconds`
+  taskDurationSpan.classList.add('task-li-duration')
 
-  task.appendChild(taskName)
-  task.appendChild(taskDuration)
+  task.appendChild(taskNameSpan)
+  task.appendChild(taskDurationSpan)
 
   return task
 }
 
-function createTaskLiContent({taskName, duration}){
-  let taskName = document.createElement('span')
-  taskName.innerText = t.taskName
-  taskName.classList.add('task-li-name')
-  let taskDuration = document.createElement('span')
-  taskDuration.innerText = 0
-  taskDuration.classList.add('task-li-duration')
-  return {taskName, taskDuration}
-}
+// function createTaskLiContent({taskName, duration}){
+//   let taskName = document.createElement('span')
+//   taskName.innerText = t.taskName
+//   taskName.classList.add('task-li-name')
+//   let taskDuration = document.createElement('span')
+//   taskDuration.innerText = 0
+//   taskDuration.classList.add('task-li-duration')
+//   return {taskName, taskDuration}
+// }
 
+// const parseSpanTime = ({startTime, stopTime}) => {
+//   return {
+//     seconds: Math.floor(Math.abs((start - Date.now())/1000)) % 60,
+//     minutes: Math.floor(Math.abs((start - Date.now())/1000)/ 60)
+//     hours: Math.floor(Math.abs((start - Date.now())/1000)/ 60 / 60)
+//   }
+// }
+
+// const displaySpanTime = ({minutes, hours}) => {
+//   let spanString = ''
+//   if (minutes < 0) spanString = seconds > 1 ? `${seconds}`
+// }
